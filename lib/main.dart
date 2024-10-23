70627951d8b4c212e95c3d88aa5ba450cf0f302b
selarmld <armildasyahla@gmail.com>
2024-10-23 13:02:55 +0800

    update

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:pertemuan7/model/task_data.dart';
+import 'package:pertemuan7/screens/tasks_screen.dart';
+import 'package:pertemuan7/widget/darkmode.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (BuildContext context) => TaskData(),
+        ),
+        ChangeNotifierProvider(
+          create: (BuildContext context) => ThemeModeData(),
+        )
+      ],
+      child: Builder(builder: (context) {
+        return MaterialApp(
+          themeMode: Provider.of<ThemeModeData>(context).themeMode,
+          darkTheme: ThemeData.dark(useMaterial3: true),
+          home: TasksScreen(),
+        );
+      }),
+    );
+  }
+}
---
+++ lib/widget/task_list.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:pertemuan7/model/task_data.dart';
+import 'package:pertemuan7/widget/task_tile.dart';
+import 'package:provider/provider.dart';
+
+import '../model/task.dart';
+
+class TasksList extends StatelessWidget {
+  const TasksList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            final task = taskData.tasks[index];
+            return TaskTile(
+              taskTitle: task.name,
+              isChecked: task.isDone,
+              checkboxCallback: (checkboxState) {
+                taskData.updateTask(task);
+              },
+              longPressCallback: () {
+                taskData.deleteTask(task);
+              },
+            );
+          },
+          itemCount: taskData.taskCount,
+        );
+      },
+    );
+  }
+}
